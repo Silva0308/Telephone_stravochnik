2fe9550e71800cae8ec150090561350a8f372c7f
Valeria Vasileva <108118608+Silva0308@users.noreply.github.com>
2022-10-19 11:32:29 +0300

    Create log.py
    добавила логирование

---
+++ log.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from time import time
+from pathlib import Path
+
+
+
+def start_app(data):
+    file = Path("Python_seminars", "SEM_7", 'log.csv')
+    time_calc = datetime.now().strftime('%H:%M')
+    with open(file, 'a', encoding='utf-8') as log_file:
+        log_file.write('{} Time {}\n'.format('Start work with app', time_calc))
+
+def error_enter():
+    time_calc = datetime.now().strftime('%H:%M')
+    file = Path("Python_seminars", "SEM_7", 'log.csv')
+    with open(file, 'a', encoding='utf-8') as log_file:
+        log_file.write('{} Time {}\n'.format('Enter error', time_calc))
+
+def show_all():
+    time_calc = datetime.now().strftime('%H:%M')
+    file = Path("Python_seminars", "SEM_7", 'log.csv')
+    with open(file, 'a', encoding='utf-8') as log_file:
+        log_file.write('{} Time {}\n'.format('Showed all items', time_calc))
+
+def search(search_object):
+    time_calc = datetime.now().strftime('%H:%M')
+    file = Path("Python_seminars", "SEM_7", 'log.csv')
+    with open(file, 'a', encoding='utf-8') as log_file:
+        log_file.write('Search: {} Time {}\n'.format(search_object, time_calc))
+
+def add(added_object):
+    time_calc = datetime.now().strftime('%H:%M')
+    file = Path("Python_seminars", "SEM_7", 'log.csv')
+    with open(file, 'a', encoding='utf-8') as log_file:
+        log_file.write('Added: {} Time {}\n'.format(added_object, time_calc))
+
+def change(changed_object):
+    time_calc = datetime.now().strftime('%H:%M')
+    file = Path("Python_seminars", "SEM_7", 'log.csv')
+    with open(file, 'a', encoding='utf-8') as log_file:
+        log_file.write('Changed: {} Time {}\n'.format(changed_object, time_calc))
+
+def del_item(deleted_object):
+    time_calc = datetime.now().strftime('%H:%M')
+    file = Path("Python_seminars", "SEM_7", 'log.csv')
+    with open(file, 'a', encoding='utf-8') as log_file:
+        log_file.write('Deleted: {} Time {}\n'.format(deleted_object, time_calc))
+
+def end_app():
+    file = Path("Python_seminars", "SEM_7", 'log.csv')
+    time_calc = datetime.now().strftime('%H:%M')
+    with open(file, 'a', encoding='utf-8') as log_file:
+        log_file.write('{} Time {}\n'.format('End work with app', time_calc))
