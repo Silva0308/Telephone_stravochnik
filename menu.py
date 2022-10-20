6a6dbd9299c3e7afd86ee2cde0dfdb1b7ec9fd43
Valeria Vasileva <108118608+Silva0308@users.noreply.github.com>
2022-10-20 21:29:45 +0300

    уже с логами

--- menu.py
+++ menu.py
@@ -4,9 +4,11 @@ from exceptions import user_choice
 from file_writing import writing_txt, writing_scv
 import get_data
 from add_info import adding
+import log
 
 
 def input_contact_menu_choice():
+    log.start_app()
     while True:
         print()
         print('-----------------------')
@@ -29,20 +31,26 @@ def input_contact_menu_choice():
             choice1 = user_choice()
             if choice1 == 1:
                 print_csv()
+                log.show_all()
             if choice1 == 2:
                 print_txt()
+                log.show_all()
             if choice1 == 3:
                 print_all()
+                log.show_all()
             else:
                 return input_contact_menu_choice()
         elif choice_menu == 2:
-            look()
+            search_object = look()
+            log.search(search_object)
         elif choice_menu == 3:
-            adding()
+            to_add = adding()
+            log.add(to_add)
         elif choice_menu == 4:
             return 4
         elif choice_menu == 5:
-            delete_contact()
+            del_key = delete_contact()
+            log.del_item(del_key)
         elif choice_menu == 6:
             phonebook = get_data.data_entry()
             writing_scv(phonebook)
@@ -50,8 +58,10 @@ def input_contact_menu_choice():
             new_key = max(phonebook)
             with open('last_key.txt', "w", encoding='utf-8') as my_f:
                 my_f.write(f"last_key = {new_key}")
+            log.new_book()
 
         elif choice_menu == 0:
+            log.end_app()
             return exit()
         else:
             return input_contact_menu_choice()
